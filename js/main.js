4cd64fa3e8d8ee9339fe98e35dc832f545d48edf
user <kaibuchholz13@gmail.com>
2022-03-12 22:21:41 +0100

    variablen FrLm1- FrLm4

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 let Kalorienziel;
+let FrLm1;
+let FrLm2;
+let FrLm3;
+let FrLm4;
+
+
+
 
 function tfAuslesen() {
     Kalorienziel = parseInt(document.getElementById("tfKalorienziel").value);
+    FrLm1 = parseInt(document.getElementById("tfFrühstück1"));
+    FrLm2 = parseInt(document.getElementById("tfFrühstück1"));
+    FrLm3 = parseInt(document.getElementById("tfFrühstück1"));
+    FrLm4 = parseInt(document.getElementById("tfFrühstück1"));
 
 }
