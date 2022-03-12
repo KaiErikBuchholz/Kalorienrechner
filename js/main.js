1b0f71eea5dbaca367b757eb4a15960e188bd83d
user <kaibuchholz13@gmail.com>
2022-03-12 22:48:58 +0100

    variablen FrLm1 - AbLm4

--- js/main.js
+++ js/main.js
@@ -3,6 +3,16 @@ let FrLm1;
 let FrLm2;
 let FrLm3;
 let FrLm4;
+let MiLm1;
+let MiLm2;
+let MiLm3;
+let MiLm4;
+let AbLm1;
+let AbLm2;
+let AbLm3;
+let AbLm4;
+
+
 
 
 
@@ -10,8 +20,21 @@ let FrLm4;
 function tfAuslesen() {
     Kalorienziel = parseInt(document.getElementById("tfKalorienziel").value);
     FrLm1 = parseInt(document.getElementById("tfFrühstück1"));
-    FrLm2 = parseInt(document.getElementById("tfFrühstück1"));
-    FrLm3 = parseInt(document.getElementById("tfFrühstück1"));
-    FrLm4 = parseInt(document.getElementById("tfFrühstück1"));
+    FrLm2 = parseInt(document.getElementById("tfFrühstück2"));
+    FrLm3 = parseInt(document.getElementById("tfFrühstück3"));
+    FrLm4 = parseInt(document.getElementById("tfFrühstück4"));
+    MiLm1 = parseInt(document.getElementById("tfMittag1"));
+    MiLm2 = parseInt(document.getElementById("tfMittag2"));
+    MiLm3 = parseInt(document.getElementById("tfMittag3"));
+    MiLm4 = parseInt(document.getElementById("tfMittag4"));
+    AbLm1 = parseInt(document.getElementById("tfAbendbrot1"));
+    AbLm2 = parseInt(document.getElementById("tfAbendbrot2"));
+    AbLm3 = parseInt(document.getElementById("tfAbendbrot3"));
+    AbLm4 = parseInt(document.getElementById("tfAbendbrot4"));
+
+
+
+
+
 
 }
